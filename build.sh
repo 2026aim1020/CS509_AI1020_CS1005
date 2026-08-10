69f5bff04d0ed79c8e2598ab1e5ab9ec993cee6f
unknown <2026aim1020@iitrpr.ac.in>
2026-08-11 00:39:12 +0530

    added build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,34 @@
+
+g++ -O2 -std=c++17 -static-libgcc -static-libstdc++ \
+  -Iassignment_01/include \
+  -Iassignment_02/include \
+  assignment_01/src/csr.cpp \
+  assignment_01/src/graph_algorithms.cpp \
+  assignment_02/src/graph_analytics.cpp \
+  assignment_02/src/bc_driver.cpp \
+  -o assignment_02/bc_driver
+
+
+g++ -O2 -std=c++17 -static-libgcc -static-libstdc++ \
+  -Iassignment_01/include \
+  -Iassignment_02/include \
+  assignment_01/src/csr.cpp \
+  assignment_01/src/graph_algorithms.cpp \
+  assignment_02/src/graph_analytics.cpp \
+  assignment_02/src/cc_driver.cpp \
+  -o assignment_02/cc_driver
+
+
+g++ -O2 -std=c++17 -static-libgcc -static-libstdc++ \
+  -Iassignment_01/include \
+  -Iassignment_02/include \
+  assignment_01/src/csr.cpp \
+  assignment_01/src/graph_algorithms.cpp \
+  assignment_02/src/graph_analytics.cpp \
+  assignment_02/src/tc_driver.cpp \
+  -o assignment_02/tc_driver
+
+
 g++ -O2 -std=c++17 -static-libgcc -static-libstdc++ -pthread \
   -Iassignment_01/include \
   -Iassignment_02/include \
