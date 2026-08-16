e3718a60d29df74a9c259cf2c629080edd54be23
2026csm1005-gif <2026csm1005@iitrpr.ac.in>
2026-08-16 17:06:53 +0530

    Add header file for Maxflow-Mincut

---
+++ assignment_03/include/maxflow.h
@@ -0,0 +1,21 @@
+#ifndef MAXFLOW_H
+#define MAXFLOW_H
+
+#include "csr.h"
+#include <string>
+#include <tuple>
+#include <vector>
+
+struct MaxflowResult
+{
+    double max_flow = 0.0;
+    std::vector<int> source_side;
+    std::vector<int> sink_side;
+    std::vector<std::tuple<int, int, double>> cut_edges;
+};
+
+bool read_maxflow_input(const std::string &path, AdjacencyList &g, int &sink, std::string &error);
+
+MaxflowResult dinic_maxflow(const CSR &csr, int source, int sink);
+
+#endif
